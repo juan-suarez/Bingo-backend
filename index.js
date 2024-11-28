1e1d272769fc52b0c88d0a35d2b6ef5d5aed855a
juan-suarez <juan.suarez1@utp.edu.co>
2024-11-28 10:38:44 -0500

    Initial server configuration

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express();
+
+const port = process.env.PORT ?? 3000;
+
+app.get('/',(req,res) => {
+  res.send('app is healty')
+})
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
